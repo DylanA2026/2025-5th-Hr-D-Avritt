4991d9876259d4332253c734a118c157e19b46d5
Dylan Avritt <da125005@student.wpsok.org>
2025-08-20 12:52:45 -0500

    Added HW2 Math & Syntax

---
+++ HW2.py
@@ -0,0 +1,40 @@
+#Name: Dylan Avritt
+#Class: 5th Hour
+#Assignment: HW2
+import math
+
+#1. Print "Hello World!"
+print("Hello World")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+RandomNumber = 57
+Greeting = "Hello Everyone"
+IsPersonAwesome = True
+
+#3. Print all three variables on the same print function (at the same time).
+
+print(RandomNumber, Greeting, IsPersonAwesome)
+
+#4. Create a variable that asks the user to input an integer.
+firstNumber = int(input("First Number: "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+sum = RandomNumber + firstNumber
+print (sum)
+
+   #6. Take the result from #5 and divide it by 2. Print the result.
+print (sum / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+IsPersonAwesome = False
+#8. Print the value of the boolean variable.
+print(IsPersonAwesome)
+#9. Create a variable with a number that contains decimals.
+DecimalNumber = 3.14
+#10. Round the number from #9 up or down using the round function.
+print (round(DecimalNumber))
+
+
+
+
+
+
+

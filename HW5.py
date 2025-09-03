3584e7fe5ebc723fc7606ceb97b5c6271db88df0
Dylan Avritt <da125005@student.wpsok.org>
2025-09-03 12:59:42 -0500

    Finished HW 5

--- HW5.py
+++ HW5.py
@@ -1,22 +1,31 @@
-#Name: Dylan Avritt
+#Name:
 #Class: 5th Hour
 #Assignment: HW5
 
 
 #1. Create a list with 9 different numbers inside.
-
-#2. Sort the list.
-
+NumList = [81, 21, 33, 4, 54, 6, 47, 18, 69]
+#2. Sort the list from highest to lowest.
+NumList.sort(reverse=True)
 #3. Create an empty list.
-
-#4. Remove the lowest, highest, and median number from the first list.
-
-#5. Add them to the empty list.
-
+EmptyList = []
+#4. Remove the median number from the first list and add it to the second list.
+EmptyList.append(NumList[4])
+NumList.pop(4)
+#5. Remove the first number from the first list and add it to the second list.
+EmptyList.append(NumList[0])
+NumList.pop(0)
 #6. Print both lists.
-
-#7. Add the three numbers in the second list together and print the result.
-
+print(NumList)
+print(EmptyList)
+#7. Add the two numbers in the second list together and print the result.
+print(EmptyList[0] + EmptyList[1])
 #8. Move the number back to the first list (like you did in #4 and #5 but reversed).
-
-#9. Print the first list. to the empty list.
+NumList.append(EmptyList[1])
+EmptyList.pop(1)
+#5. Remove the first number from the first list and add it to the second list.
+NumList.append(EmptyList[0])
+EmptyList.pop(0)
+#9. Sort the first list from lowest to highest and print it.
+NumList.sort()
+print(NumList)

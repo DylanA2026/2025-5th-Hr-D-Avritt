05368d2b6b51d675769775245ab7da5f28c661f4
Dylan Avritt <da125005@student.wpsok.org>
2025-09-23 13:06:12 -0500

    Started SC1 (Not Finished)

---
+++ SC1.py
@@ -0,0 +1,43 @@
+#Name:
+#Class: 5th Hour
+#Assignment: Scenario 1
+
+#Scenario 1:
+#You are a programmer for a fledgling game developer. Your team lead has asked you
+#to create a nested dictionary containing five enemy creatures (and their properties)
+#for combat testing. Additionally, the testers are asking for a way to input changes
+#to the enemy's damage values for balancing, as well as having it print those changes
+#to confirm they went through.
+
+#It is up to you to decide what properties are important and the theme of the game.
+
+print("Hello World!")
+
+Creatures = {
+    "Creature_1" : {
+        "Class" : "Support",
+        "Damage" : 10,
+        "Cooldown" : 20
+    },
+    "Creature_2" : {
+        "Class" : "Entry",
+        "Damage" : 40,
+        "Cooldown" : 50,
+    },
+    "Creature_3" : {
+        "Class" : "Support",
+        "Damage" : 25,
+        "Cooldown" : 30
+    },
+    "Creature_4": {
+        "Class": "Anchor",
+        "Damage" : 80,
+        "Cooldown" : 75
+    },
+    "Creature_5": {
+        "Class": "Intel",
+        "Damage" : 35,
+        "Cooldown" : 35
+    },
+}
+

e8c50cf644ec7d509994ae751be30d66d21da295
Dylan Avritt <da125005@student.wpsok.org>
2025-10-08 12:44:51 -0500

    Edited HW9

--- HW9.py
+++ HW9.py
@@ -6,7 +6,7 @@ import random
 #1. Print "Hello World!"
 print("Hello World!")
 #2. Create a list with three variables that each randomly generate a number between 1 and 100
-numberList = (random.randint(1,100), random.randint(1,100), random.randint(1,100))
+numberList = [random.randint(1,100), random.randint(1,100), random.randint(1,100)]
 #3. Print the list.
 print(numberList)
 #4. Create an if statement that determines which of the three numbers is the highest and prints the result.
